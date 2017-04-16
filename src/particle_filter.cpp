246dd38b11f08dcc4f457405ddcbdbe031111683
Alex <alexey.simonov@gmail.com>
2017-04-16 12:32:10 +0100

    clean up code and comments

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -45,18 +45,18 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   is_initialized = true;
 }
 
-// Add measurements to each particle and add random Gaussian noise.
+// Apply movement equation to each particle and add random Gaussian noise.
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
   random_device rd;
   default_random_engine gen(rd());
-  normal_distribution<float> N_x(0, std_pos[0]);
-  normal_distribution<float> N_y(0, std_pos[1]);
-  normal_distribution<float> N_theta(0, std_pos[2]);
+  normal_distribution<double> N_x(0, std_pos[0]);
+  normal_distribution<double> N_y(0, std_pos[1]);
+  normal_distribution<double> N_theta(0, std_pos[2]);
 
   Particle p;
   // optimize calculation outside the loop
-  float vy = fabs(yaw_rate)>1e-4 ? velocity / yaw_rate : velocity;
-  float tdt = yaw_rate * delta_t;
+  double vy = velocity / yaw_rate;
+  double tdt = yaw_rate * delta_t;
   // loop over all particles
   for (int i=0; i<num_particles; i++) {
     p = particles[i];
@@ -70,6 +70,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
       p.y += velocity * delta_t * sin(p.theta) + N_y(gen);
     }
     p.theta = thetanew + N_theta(gen);
+
     particles[i] = p; // overwrite particle with new state
   }
 
@@ -77,32 +78,29 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 
 
 
+
+void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
+// unused. all calculations are done in updateWeights
+}
+
+
 // x_map and y_map will have x and y translated to map coordinates
 void CarToMap(double x, double y, double x_car_map, double y_car_map, double phi_car_map, double& x_map, double & y_map)
 {
   // using http://planning.cs.uiuc.edu/node99.html
+  // Assuming map x axis points right, y axis points up.
+  // Assuming car x axis points forward, y axis points left.
   x_map = x * cos(phi_car_map) - y * sin(phi_car_map) + x_car_map;
   y_map = x * sin(phi_car_map) + y * cos(phi_car_map) + y_car_map;
 }
 
-void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-  // TODO: Find the predicted measurement that is closest to each observed measurement and assign the
-  //   observed measurement to this particular landmark.
-  // NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
-  //   implement this method and use it as a helper during the updateWeights phase.
 
-}
-
-// calculate 2D-gaussian probability with 0 correlation
+// calculate 2D-gaussian probability density with 0 correlation
 double Gaussian2DNoCorrelation(double x, double mu_x, double y, double mu_y, double sigma_x, double sigma_y)
 {
   return (1./(2.*M_PI*sigma_x*sigma_y)) * exp( - ( pow(x-mu_x, 2)/(2*sigma_x*sigma_x) + pow(y-mu_y, 2)/(2*sigma_y*sigma_y) ) );
 }
-// calculate 2D-gaussian log(probability) with 0 correlation
-double LogGaussian2DNoCorrelation(double x, double mu_x, double y, double mu_y, double sigma_x, double sigma_y)
-{
-  return - log(2.*M_PI*sigma_x*sigma_y) - ( pow(x-mu_x, 2)/(2*sigma_x*sigma_x) + pow(y-mu_y, 2)/(2*sigma_y*sigma_y) ) ;
-}
+
 
 // calculate distance between 2 LandmarkObs objects
 double ObservationsDistance(LandmarkObs& obs1, LandmarkObs& obs2)
@@ -111,7 +109,7 @@ double ObservationsDistance(LandmarkObs& obs1, LandmarkObs& obs2)
 }
 
 
-// Update the weights of each particle using a mult-variate Gaussian distribution
+// Update the weights of each particle using a multi-variate Gaussian distribution
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], std::vector<LandmarkObs> observations, Map map_landmarks) {
 
   // if there are no observations do nothing as we have no new information to incorporate into weights
@@ -159,16 +157,16 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], s
     // using approach: http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
     vector<int> sort_idx(close_landmark_distances.size());
     std::iota(sort_idx.begin(), sort_idx.end(), 0); // original indices 0, 1, 2...
+    // sort using lambda function
     std::sort(sort_idx.begin(), sort_idx.end(),
               [&close_landmark_distances](int i1, int i2){return close_landmark_distances[i1]<close_landmark_distances[i2];});
     // do the nearest neighbour and calculate probabilities at the same time
     // cummulative probability for this particle for all observations
     double prob = 1.0;
-    // probability for landmark missing in the measurements. assume it is at sensor range. should give really small probability
     for (int l=0; l<close_landmarks.size(); l++)
     {
       LandmarkObs lm = close_landmarks[sort_idx[l]/*using sort by distance*/];
-      // note: observations are removed from observations list (our own copy in this function)
+      // note: observations are removed from observations list (own copy in this function)
       double dist, min_dist = numeric_limits<double>::max();
       int min_o = -1;
       for (int o=0; o<observations_map_coord.size(); o++)
@@ -183,18 +181,14 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], s
       if (min_o>-1)
       {
         // we matched landmark to observation. calculate probability and remove that observation from further consideration
-        // we will be summing log likelyhoods
         prob *= Gaussian2DNoCorrelation(observations_map_coord[min_o].x, lm.x,
                                         observations_map_coord[min_o].y, lm.y,
                                         std_landmark[0], std_landmark[1]);
         observations_map_coord.erase(observations_map_coord.begin()+min_o); // this is horrible in terms of performance as it has to shift remaining elements
-        // let try to base our probability likelyhood on just one landmark 'match', the closest one
-        //if (l>=1) break; // dont consider other landmarks
       } else
       {
         // this is the case we have run out of observations to match.
         // one way here is to assign minimum probability to this landmarks. but that's not good. it will produce zero total probability
-        //prob *= min_prob;
         // instead we just ignore this landmark. may be it was occluded or there is fog and sensor cannot see it or whatever.
         // let's just focus on landmarks we can match
       }
@@ -210,6 +204,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], s
       sum_weights += p.weight;
     } else
       p.weight = 0.;
+
     particles[i] = p;
   } // loop over particles
 
@@ -217,12 +212,12 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], s
     cout << "VERY SMALL WEIGHTS" << endl;
 
   if (sum_weights == 0)
-    cout << "ZERO WEIGHTS SUM. KAPUT!" << endl;
+    cout << "ZERO WEIGHTS SUM. PROBABLY KAPUT!" << endl;
 
   //normalize weights
   double max_weight = 0.;
   for (int i=0; i<num_particles; i++) {
-    particles[i].weight /= sum_weights > 1e-14 ? sum_weights : 1e-14;
+    particles[i].weight /= (sum_weights > 1e-14 ? sum_weights : 1e-14);
     if (particles[i].weight > max_weight)
       max_weight = particles[i].weight;
     weights[i] = particles[i].weight;
@@ -250,8 +245,8 @@ void ParticleFilter::resample() {
   particles = particles_new;
 }
 
+// write particles to the specified file
 void ParticleFilter::write(std::string filename, double gt_x, double gt_y, double gt_theta) {
-  // You don't need to modify this file.
   std::ofstream dataFile;
   dataFile.open(filename, std::ios::app);
   for (int i = 0; i < num_particles; ++i) {
